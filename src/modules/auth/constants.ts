be6b9bcb2b4d16bcd8f6dfccd0ac1a82f092cd36
Grupo5Sena <miller52836y@gmail.com>
2025-09-27 17:26:37 -0500

    Primer commit proyecto marketplace

--- src/modules/auth/constants.ts
+++
@@ -1,4 +0,0 @@
-export const JWT_SECRET = process.env.JWT_SECRET || 'your-super-secret-jwt-key-change-in-prod';
-export const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '15m';
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'your-super-secret-refresh-key';
-export const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION || '7d';
--- src/modules/auth/dto/tokens.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class TokensDto {
-  accessToken: string;
-  refreshToken: string;
-}
